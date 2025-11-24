a7981e7d394b6c7d4ed99ca99ed7bc2abc2021b0
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-24 17:53:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('stub');
+import express from "express";
+import cors from "cors";
+import processClip from "./routes/processClip.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/process", processClip);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("FFmpeg worker running on port", port));
